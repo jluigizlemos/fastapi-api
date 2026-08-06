a5aa83c96f34524b74947934d1414d78d4a46ddf
Luigi Lemos <luigi@example.com>
2026-08-06 17:35:08 -0300

    Implementando JWc:\Users\luigi\Desktop\FastApi\auth_routes.py

--- auth_routes.py
+++ auth_routes.py
@@ -2,26 +2,40 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario, db
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY, bcrypt_context
 from schemas import LoginSchema, UsuarioSchema
-
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-def criar_token(id_usuario):
-    """
-    Função para criar um token de autenticação
-    """
-    token= f"ffkwmnk85423{id_usuario}"
-    return token
+def criar_token(id_usuario, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    #JWT 
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+
+def autenticar_usuario(email, senha, session: Session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+    
+    return usuario
+
+
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
+    return usuario
+
 
 
 @auth_router.get("/")
 async def autenticar():
-    """
-    Essa é a rota padrão de autenticação do nosso sistema
-    """
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
 
@@ -40,11 +54,20 @@ async def criar_conta(usuario_schema: UsuarioSchema, session=Depends(pegar_sessa
 
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session=Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
-        raise HTTPException(status_code=400, detail="Email de usuário não encontrado")
+        raise HTTPException(status_code=400, detail="Email de usuário não encontrado ou credenciais inválidas")
     else:
         access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return {"access_token": access_token,
+                "refresh_token": refresh_token,
                 "token_type": "Bearer"}
-        
+
+
+@auth_router.get("/refresh")
+async def use_refresh_token(token: str):
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return {"access_token": access_token,
+            "token_type": "Bearer"}
