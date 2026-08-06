d6fbcbd6a83b9321be7ff9372bdf81090d253e13
Luigi Lemos <luigi@example.com>
2026-08-06 15:51:01 -0300

    Criando Pedido e Login

--- auth_routes.py
+++ auth_routes.py
@@ -3,10 +3,19 @@ from models import Usuario, db
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import LoginSchema, UsuarioSchema
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+def criar_token(id_usuario):
+    """
+    Função para criar um token de autenticação
+    """
+    token= f"ffkwmnk85423{id_usuario}"
+    return token
+
 
 @auth_router.get("/")
 async def autenticar():
@@ -26,6 +35,16 @@ async def criar_conta(usuario_schema: UsuarioSchema, session=Depends(pegar_sessa
         novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
+        return {"mensagem": f"usuário cadastrado com sucesso{usuario_schema.email}"}
 
-    
-    return {"mensagem": f"usuário cadastrado com sucesso{usuario_schema.email}"}
+
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session=Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Email de usuário não encontrado")
+    else:
+        access_token = criar_token(usuario.id)
+        return {"access_token": access_token,
+                "token_type": "Bearer"}
+        
